819fd681c5dfa7fbac085817a0d28f72b4d0d0bf
D4rkHeart <89066588+D4rkHeart@users.noreply.github.com>
2022-02-15 17:38:35 +0100

    [feat] Added creation restaurant rooter#2

---
+++ newRestaurant.php
@@ -0,0 +1,6 @@
+<?php
+include_once 'database.php';
+$runningAction = "createRestaurant.php";
+$submitText = "Créer";
+$restaurant = array("restaurant_nom" => "","restaurant_pays" => "","restaurant_type" => "");
+include_once 'restaurantForm.php';
